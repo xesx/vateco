8f055f5fc41ff2fb109c8bda1517e2724841a10e
Alexey Alekhin <alekhin@sportxline.com>
2025-09-05 11:17:08 +0300

    cli install comfyui start dev 3

--- apps/app-cli/src/command/install-comfyui-v0.cli.ts
+++ apps/app-cli/src/command/install-comfyui-v0.cli.ts
@@ -11,14 +11,19 @@ export class InstallComfyuiV0Cli {
   register(program) {
     program
       .command('install-comfyui-v0')
-      .description('Сказать привет')
+      .description('install comfyui v0')
       .action(async () => {
-        console.log('in install comfyui v0 cli')
-        const version = await this.rclonesrv.getRcloneVersion()
-        console.log('\x1b[36m', 'res', version, '\x1b[0m')
+        try {
+          console.log('in install comfyui v0 cli')
+          const version = await this.rclonesrv.getRcloneVersion()
+          console.log('\x1b[36m', 'res', version, '\x1b[0m')
 
-        const list = await this.rclonesrv.operationsList()
-        console.log('\x1b[36m', 'list', list, '\x1b[0m')
+          const list = await this.rclonesrv.operationsList()
+          console.log('\x1b[36m', 'list', list, '\x1b[0m')
+        } catch (error) {
+          console.error('Error during install-comfyui-v0:', error)
+        }
+
       })
   }
 }
---
+++ bin/vast-ai-provising-script.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+RCLONE_CONFIG_DIR="${HOME}/.config/rclone"
+RCLONE_CONFIG_FILE="${RCLONE_CONFIG_DIR}/rclone.conf"
+
+git clone https://github.com/xesx/vastai-scripts.git
+git clone https://github.com/xesx/vateco.git
+
+# Импортируем секреты из Infisical
+source ./vastai-scripts/import-secrets.sh
+
+# Packages are installed after nodes so we can fix them...
+
+APT_PACKAGES=(
+)
+
+# Make rclone config
+mkdir -p "${RCLONE_CONFIG_DIR}"
+
+cat > "${RCLONE_CONFIG_FILE}" <<EOF
+[ydisk]
+type = yandex
+token = {"access_token":"$YANDEX_DISK_ACCESS_TOKEN","token_type":"OAuth","refresh_token":"$YANDEX_DISK_REFRESH_TOKEN","expiry":"2025-04-15T14:11:36.588423+03:00"}
+EOF
+
+# Запуск rclone API-сервера без авторизации на порту 5572
+nohup rclone rcd --rc-addr=:5572 --rc-no-auth > /var/log/rclone.log 2>&1 &
+
+function provisioning_start() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+
+    provisioning_get_apt_packages
+
+#    rclone config reconnect ydisk:
+#    rclone copy -P ydisk:comfyui-link-source/user /workspace/ComfyUI/user
+
+    deploy_app_cloud_api
+
+    vateco-cli install-comfyui-v0
+
+    printf "\nProvisioning complete:  Application will start now\n\n"
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+            sudo apt install "${APT_PACKAGES[@]}"
+    fi
+}
+
+deploy_app_cloud_api() {
+    cd ${WORKSPACE}/vateco
+
+    source /opt/nvm/nvm.sh
+    nvm use 22
+
+    # Установка зависимостей
+    npm install
+
+    # Сборка проекта
+    npm run build
+    npm link
+
+    # Запуск приложения cloud-app-api
+    nohup npm run start:cloud-api:prod > cloud-app-api.log 2>&1 &
+
+    # Запуск приложения cloud-app-cron
+    nohup npm run start:cloud-cron:prod > cloud-app-cron.log 2>&1 &
+
+    cd ${WORKSPACE}
+}
+
+provisioning_start
