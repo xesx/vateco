dcccfcc0718ebd25d15f0c8bdce7f20ca369854f
Alexey Alekhin <alekhin@sportxline.com>
2025-09-21 20:54:17 +0300

    aff sd15 models

--- model/index.ts
+++ model/index.ts
@@ -1,7 +1,9 @@
 import * as clip from './clip.model.json'
 import * as flux from './flux.model.json'
+import * as sd15 from './sd15.checkpoint.model.json'
 
 export default {
   ...clip,
   ...flux,
+  ...sd15,
 }
