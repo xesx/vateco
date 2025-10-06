1c48ee4a6074f3c3b2b32e06fad4815cb92965e8
Alexey Alekhin <alekhin@sportxline.com>
2025-10-06 18:42:55 +0300

    amvera deploy dev 6

--- bin/amvera-build.sh
+++ bin/amvera-build.sh
@@ -13,10 +13,16 @@ API_URL="https://us.infisical.com/api/v3/secrets/raw"
 # Получаем JSON с секретами
 response=$(curl -s -H "Authorization: Bearer $INFISICAL_TOKEN" "$API_URL")
 
+echo "--->>>>>>>>>INFISICAL_TOKEN $INFISICAL_TOKEN"
+echo "--->>>>>>>>>DATABASE_URL $DATABASE_URL"
+
 echo "$response" | jq -r '.secrets[] | "\(.secretKey)=\(.secretValue)"' > .env
 source .env
 cat .env
 echo "Секреты загружены:"
+
+echo "--->>>>>>>>>INFISICAL_TOKEN $INFISICAL_TOKEN"
+echo "--->>>>>>>>>DATABASE_URL $DATABASE_URL"
 
 npm run migrate:deploy
 echo "Migrations deployed successfully"
