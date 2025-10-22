56df8bcfc9bb43142e13188d0a135b461433fae8
Alexey Alekhin <alekhin@sportxline.com>
2025-10-22 15:28:33 +0300

    il wf enum models

--- model/index.ts
+++ model/index.ts
@@ -2,10 +2,12 @@ import * as clip from './clip.model.json'
 import * as flux from './flux.model.json'
 import * as sd15 from './sd15.checkpoint.model.json'
 import * as sdxl from './sdxl.checkpoint.model.json'
+import * as illustrious from './illustrious.checkpoint.model.json'
 
 export default {
   ...clip,
   ...flux,
   ...sd15,
   ...sdxl,
+  ...illustrious,
 }
