a6336718a71281f456c1e0886e2c0be7d5be25ee
Alexey Alekhin <alekhin@sportxline.com>
2026-06-27 16:51:53 +0300

    download restart every minute

--- libs/huggingface/src/huggingface.lib.service.ts
+++ libs/huggingface/src/huggingface.lib.service.ts
@@ -15,22 +15,48 @@ export class HuggingfaceLibService {
 
   async download ({ repo = 'alalarty/models2', filename, dir }) {
     const env = { ...process.env, HF_HUB_ENABLE_HF_TRANSFER: '1' }
+    const timeoutMs = 60_000
 
-    await new Promise((resolve, reject) => {
-      const child = spawn('hf', ['download', repo, filename, '--local-dir', dir], { env })
+    while (true) {
+      const isCompleted = await new Promise<boolean>((resolve, reject) => {
+        const child = spawn('hf', ['download', repo, filename, '--local-dir', dir], { env })
+        let isTimedOut = false
 
-      child.stdout.setEncoding('utf8')
-      child.stderr.setEncoding('utf8')
+        child.stdout.setEncoding('utf8')
+        child.stderr.setEncoding('utf8')
 
-      child.on('close', (code) => {
-        if (code === 0) {
-          resolve(true)
-        } else {
+        const timer = setTimeout(() => {
+          isTimedOut = true
+          child.kill('SIGKILL')
+        }, timeoutMs)
+
+        child.on('error', (error) => {
+          clearTimeout(timer)
+          reject(error)
+        })
+
+        child.on('close', (code) => {
+          clearTimeout(timer)
+
+          if (isTimedOut) {
+            resolve(false)
+            return
+          }
+
+          if (code === 0) {
+            resolve(true)
+            return
+          }
+
           const error = new Error(`HuggingfaceLibService_download_87 hf download exited with code ${code}`)
           reject(error)
-        }
+        })
       })
-    })
+
+      if (isCompleted) {
+        return
+      }
+    }
   }
 
   async downloadWithRetry ({ repo = 'alalarty/models2', filename, dir, retries = 3, delayMs = 5000 }) {
